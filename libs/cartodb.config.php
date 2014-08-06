9055e7fd0440c3ef502162f0bc54324aea027643
Nicolás Lound <nicolas.lound@gmail.com>
2014-08-06 12:12:26 -0300

    Create cartodb.config.php
    reemplazar por los valores del usuario de cartoDB

---
+++ libs/cartodb.config.php
@@ -0,0 +1,14 @@
+<?php
+
+function getConfig() {
+  $config = array();
+
+  $config['key'] = 'key';
+  $config['secret'] = 'secret';
+  $config['email'] = 'email';
+  $config['password'] = 'password';
+  $config['subdomain'] = 'subdomain';
+
+  return $config;
+
+?>
