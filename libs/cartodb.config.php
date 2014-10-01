9ee1082c2aa1c57da2e73c52671c037b0d06c622
Davo <davidgalavotti@gmail.com>
2014-10-01 11:45:45 -0300

    Agrego topojson de las calles segmentadas

---
+++ libs/cartodb.config.php
@@ -0,0 +1,14 @@
+<?php
+
+function getConfig() {
+  $config = array();
+
+  $config['key'] = '889EV2PNSBfBRzaphSGjxhwBU7R4hWfxulr25zSF';
+  $config['secret'] = '7nXu5OLQeTSbtfA4X1EeW8VwGHw0VSpIQ6V4izP0';
+  $config['email'] = 'davogalavotti@gmail.com';
+  $config['password'] = '123qaz';
+  $config['subdomain'] = 'davo';
+
+  return $config;
+
+?>
