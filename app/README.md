2afc71c6f68d22046243c1a496180cba7ba349a2
root <root@localhost.localdomain>
2014-10-02 16:05:42 -0300

    agrego mapa al index

--- app/README.md
+++ app/README.md
@@ -1,2 +1,5 @@
 npm install
 DEBUG=* node app.js
+
+client side 
+localStorage.debug="socket.io-client:socket"
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,6 +6,6 @@
 exports.index = function(req, res){
   res.render('index', { 
   	h1: 'App - Real Time, Campanas.',
-  	title: 'lolo'
+  	title: 'App - Real Time, Campanas.',
   });
 };
