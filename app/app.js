798eaa9cd356f8e0b340b05b3c837459ad09df95
root <root@localhost.localdomain>
2014-10-03 13:03:25 -0300

    aislo socket de app.js y lo modularizo como un services

--- app/app.js
+++ app/app.js
@@ -7,9 +7,6 @@ var express = require('express');
 var routes = require('./routes/routes.js');
 var http = require('http');
 var path = require('path');
-var CartoDB = require('cartodb');
-var secret = require('./secrets.js');
-
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
@@ -24,9 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.compress({
     filter: function (req, res) {
       return /json|text|javascript|css/.test(res.getHeader('Content-Type'));
@@ -39,41 +34,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// routas y socket
 app.get('/', routes.index);
 app.get('/mapa', routes.mapa);
+require('./services/socket.js')(io);
 
-var client = new CartoDB({
-	user:secret.user,
-	api_key:secret.api_key
-});
-
-var change = function(rows){
-	var len = rows.length;
-	var prendidos = [];
-	for(var i=0;i<len;i++){
-		if (rows[i].status == 1){
-			prendidos.push(rows[i])
-		}
-	}
-	return prendidos;
-}
-
-io.sockets.on('connection', function(socket){
-	socket.emit('connected');
-	setInterval(function(){
-		client.on('connect', function(){
-			client.query("SELECT * FROM campanas_colocadas", function(err, data){
-				console.log("emit update...");
-//				socket.emit('update', data.rows);
-				socket.emit("update", change(data.rows));
-				console.log("updated emited");
-			});
-		});
-		client.connect()
-		//client.pipe(file)
-	}, 30000);
-});
-
+// listen app
 server.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ app/services/secrets.js
@@ -0,0 +1,7 @@
+module.exports = {
+	user: "bahackaton",
+	pass : "",
+	api_key:"9717f50be6513a142a5c22b8d02df19bb0bacc0a",
+	consumer_key:"CnMO6hdLFYxGsOKUsBtbRp2uJxBVJLLSn8kBdJNS",
+	consumer_secret:"5lzjPHp1LyZMO6U1FbZ9NpXVifzxJlCZc6j5nrvx"
+}
