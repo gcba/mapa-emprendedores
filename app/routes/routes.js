4bb03ecfb8a02c143e8c6823ef7e0c2079582681
root <root@localhost.localdomain>
2014-10-08 11:04:37 -0300

    arreglo rutas en login

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -7,18 +7,18 @@ module.exports = function(app){
 	// GET home page.
 	
 
-	app.get('/login', function(req, res) {
+	app.get('/', function(req, res) {
 		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
 
 	// process the login form
-	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/', // redirect to the secure profile section
-		failureRedirect : '/login', // redirect back to the signup page if there is an error
+	app.post('/', passport.authenticate('local-login', {
+		successRedirect : '/index', // redirect to the secure profile section
+		failureRedirect : '/', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
 
-	app.get('/', isLoggedIn, function(req, res){
+	app.get('/index', isLoggedIn, function(req, res){
 	  res.render('index', {
 	  	h1: 'App - Real Time, Campanas.',
 	  	title: 'App - Real Time, Campanas.',
