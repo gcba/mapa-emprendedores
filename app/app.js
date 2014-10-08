fa67c4d675333a71dd071438a357980f84640d2a
Davo <davidgalavotti@gmail.com>
2014-10-08 14:50:22 -0300

    Cambio puerto por defecto a 3001

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ var Report = mongoose.model('Report')
 mongoose.connect(config.db.url || ('mongodb://' + config.db.host + '/'+ config.db.name));
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
