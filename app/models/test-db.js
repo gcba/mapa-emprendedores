c0134cca2f9cf354533832f210294b51e4a48152
root <root@localhost.localdomain>
2014-10-09 17:38:06 -0300

    first commit

--- app/models/test-db.js
+++ app/models/test-db.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose')
 , config = require('../config')
 , User = require('./user');
 
-mongoose.connect(config.db.url || ('mongodb://' + config.db.host + '/'+ config.db.name));
-//mongoose.connect("mongodb://factorialabgcba:thelabrocks123@ds043180.mongolab.com:43180/visualizacion-cucc");
+//mongoose.connect(config.db.url || ('mongodb://' + config.db.host + '/'+ config.db.name));
+mongoose.connect("mongodb://factorialabgcba:thelabrocks123@ds043180.mongolab.com:43180/visualizacion-cucc");
 
 var newUser = new User();
 
