945fae5b0789a7ee39fe7a471cfd6c4474016ab1
root <root@localhost.localdomain>
2014-11-06 18:18:47 -0300

    agrego formatToLocalTimeDate en controllers/status

--- app/controllers/status.js
+++ app/controllers/status.js
@@ -12,11 +12,22 @@ exports.All = function(req, res) {
 
 // Mongoose: estadosegmentos.find({ updated_at: { '$gte': new Date("Thu, 06 Nov 2014 16:00:00 GMT"), '$lte': new Date("Thu, 06 Nov 2014 17:30:00 GMT") } }) { fields: undefined }  
 
+var formatToLocalTimeDate = function(inDate) {
+	var today = new Date();
+	var inDateMod = new Date(inDate);
+	offSet = today.getTimezoneOffset();
+	if(offSet < 0) {
+		inDateMod.setMinutes(inDateMod.getMinutes()+offSet );
+	} else {
+		inDateMod.setMinutes(inDateMod.getMinutes()-offSet);
+	}
+	return inDateMod;
+}
 
 exports.rangeDates = function(req, res) {
-	console.log(req.params.start)
-	console.log(req.params.end)
-	Segmentos.find({updated_at: {$gte: req.params.start, $lte: req.params.end}},  function(err, segmentos) {
+	var start = formatToLocalTimeDate(req.params.start)
+	var end = formatToLocalTimeDate(req.params.end)
+	Segmentos.find({updated_at: {$gte: start, $lte: end}},  function(err, segmentos) {
 		if (err)
 			res.send(err);
 
