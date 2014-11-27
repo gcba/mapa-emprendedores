08126bfa45797bb693aa359050baca629080ff6f
Pili Mayora <pili.mayora@gmail.com>
2014-11-27 15:33:37 -0300

    Widget de ranking por esquinas funcionando ujuu

--- dashboard/jobs/ranking.rb
+++ dashboard/jobs/ranking.rb
@@ -1,27 +1,30 @@
 require 'mysql2'
 
-begin
-	client = Mysql2::Client.new(:host => "localhost", :username => "root", :password =>"5Gonz4lol")
-	client.query("USE emergencias")
-	results = client.query("select * from fracciones_estadistica order by puntaje_ranking desc limit 10")
+SCHEDULER.every '10s' do
+	begin
+  		client = Mysql2::Client.new(:host => "localhost", :username => "root", :password =>"5Gonz4lol")
+		client.query("USE emergencias")
+		fracciones_ranking = client.query("select * from fracciones_estadistica order by puntaje_ranking desc limit 10")
 
-	results.each do |row|
-		puts row
-	end
-rescue Mysql2::Error => e
-	puts e.errno
-	puts e.error
+		items = Array.new
 
-ensure
-	client.close if client
-end
+		fracciones_ranking.each do |row|
+			esquinas_fracciones = client.query("select * from fracciones_barrios_esquinas where fraccion_id = '#{row['fraccion_id']}'")
+			esquina = ""
+			esquinas_fracciones.each do |esquina_row|
+				esquina = esquina_row["esquina"]
+			end
+			item = {:label => esquina, :value => row["puntaje_ranking"]}
+			items.push(item)
+		end
 
-# esquinas = ['CORRIENTES Y MALABIA', 'CORDOBA Y ARAOZ', 'ACOYTE Y RIVADAVIA'] 
-# buzzword_counts = Hash.new({ value: 0 })
+  		send_event('ranking', { items: items })
 
-# SCHEDULER.every '2s' do
-#   random_buzzword = buzzwords.sample
-#   buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
-  
-#   send_event('ranking', { items: buzzword_counts.values })
-# end
+	rescue Mysql2::Error => e
+		puts e.errno
+		puts e.error
+
+	ensure
+		client.close if client
+	end  	
+end 	
