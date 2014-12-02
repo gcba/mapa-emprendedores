220d9f9746524d3097e6821e6a6d8ddc1aed364a
Davo <davidgalavotti@gmail.com>
2014-12-02 16:50:00 -0300

    Agrego libreria chosen para poder cortar los barrios de un modo mas simple

--- app/public/javascripts/manejoSidebar.js
+++ app/public/javascripts/manejoSidebar.js
@@ -1,32 +1,39 @@
-//manejo los checkboxes de las capas.
+// Manejo los checkboxes de las capas.
 
 $('input:checkbox').change(
-    function(){
-    	var checkbox = this.id; 
-    	var sl = 9;
-        switch (checkbox){
-            case "luminarias": sl = 1; //layer
-                break;
-            case "nagios": sl = 2; //layer
-                break;
-            case "voluntarios": sl = 3; //layer
-                break;
+  function(){
+   var checkbox = this.id; 
+   var sl = 9;
+    switch (checkbox){
+     case "luminarias": sl = 1; //layer
+      break;
+     case "nagios": sl = 2; //layer
+      break;
+     case "voluntarios": sl = 3; //layer
+      break;
+    }
+
+   checkbox = "#"+checkbox;
+   if ($(checkbox).is(':checked')) {
+     capas[1].getSubLayer(sl).show();
+    } else {
+     capas[1].getSubLayer(sl).hide();
+    }
+
+});
+
+
+
+
+$(document).ready(function() {
+    $("#barrios").chosen({allow_single_deselect: true});
+    $(document).keyup(function(e) {
+        if (e.keyCode == 83) { 
+            $('#toggleNav').click();
+        } else if (e.keyCode == 90) {
+            console.log('Zoom?');
+            $( "#barrios_chosen" ).click();
         }
-
-        checkbox = "#"+checkbox;
-
-        if ($(checkbox).is(':checked')) {
-            capas[1].getSubLayer(sl).show();
-        }else{
-            capas[1].getSubLayer(sl).hide();
-        }
-
     });
-
-
-
-$(document).keyup(function(e) {
-    if (e.keyCode == 83) { 
-        $('#toggleNav').click();
-    } 
+    
 });
