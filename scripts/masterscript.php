b556c943a2b57334edfc911b39947e248eabc80d
Pili Mayora <pili.mayora@gmail.com>
2014-12-04 17:40:23 -0300

    primero calculo las luminarias viejas, despues corro fracciones estadistica

--- scripts/masterscript.php
+++ scripts/masterscript.php
@@ -7,10 +7,10 @@ echo("Se actualizaron las fallas de luminarias de Philips \n");
 include('luminarias_apagadas.php');
 echo("Se actualizo el estado de las luminarias en base a las fallas de Philips \n");
 
-include('fracciones_estadistica.php');
-echo("Se actualizaron las estadisticas de las fracciones en base a los cambios anteriores \n");
-
 include('luminarias_viejas.php');
 echo("Se eliminaron las luminarias que estan apagadas hace mas de 720 horas \n");
+
+include('fracciones_estadistica.php');
+echo("Se actualizaron las estadisticas de las fracciones en base a los cambios anteriores \n");
 
 ?>
