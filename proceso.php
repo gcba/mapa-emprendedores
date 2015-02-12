ea0dc979c841dde10a3a066771e9bfe7555cbf48
Nicolas Lound <nicolas.lound@gmail.com>
2015-02-12 12:45:34 -0300

    quito el echo del php

--- proceso.php
+++ proceso.php
@@ -54,7 +54,6 @@ if (!empty($_REQUEST['captcha_txt'])) {
         $campos_post = $nombre_frm . "','" . $desc_frm . "','" . $acti_frm . "','" . $tags_frm . "','" . $pendiente_revision . "','" . $tipo_frm . "','" . $sector_frm . "','" . $direccion_frm . "','" . $piso_frm . "','" . $lat_frm . "','" . $lon_frm . "','" . $mailIns_frm . "','" . $tele_frm . "','" . $web_frm . "','" . $resp_frm . "','" . $mailRes_frm . "','" . $sector_sigla . "','" . $tipo_sigla . "'";  
         $SQLQ = "INSERT%20INTO%20mapa_emprendedor%20(". $columnas_db .")%20VALUES%20(" . $geom . ",'" . $campos_post .  ")" . $api_key;
         $url = "http://baemprende.cartodb.com/api/v2/sql?q=" . $SQLQ ;
-        echo $url;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
