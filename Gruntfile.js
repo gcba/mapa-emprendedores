64f3699fd49c1e52a2d4e3c16f97d0a0816bf1b0
Nicolas Lound <nicolas.lound@gmail.com>
2015-02-19 12:04:54 -0300

    fix merges

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 /**
@@ -27,26 +26,4 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default',['watch']);
-=======
-module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      dist: {
-        files: {
-          'public/css/base.css' : 'public/css/base.scss'
-        }
-      }
-    },
-    watch: {
-      css: {
-        files: '**/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['watch']);
->>>>>>> f17898d59b9cfb0c142b24e6dd19081b7caa6686
 };
